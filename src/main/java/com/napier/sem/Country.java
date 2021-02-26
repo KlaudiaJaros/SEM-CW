9f6475f13e438ee6c0fd13db8483b772364654b1
Klaudia <56263298+klaudiajaros@users.noreply.github.com>
2021-02-26 16:19:15 +0000

    Java classes added: Country, City, Language

---
+++ src/main/java/com/napier/sem/Country.java
@@ -0,0 +1,80 @@
+package com.napier.sem;
+
+public class Country {
+    private int code;
+    private String name;
+    private String continent;
+    private String region;
+    private double surfaceArea;
+    private int population;
+    private String capital;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContinent() {
+        return continent;
+    }
+
+    public void setContinent(String continent) {
+        this.continent = continent;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public double getSurfaceArea() {
+        return surfaceArea;
+    }
+
+    public void setSurfaceArea(double surfaceArea) {
+        this.surfaceArea = surfaceArea;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "code=" + code +
+                ", name='" + name + '\'' +
+                ", continent='" + continent + '\'' +
+                ", region='" + region + '\'' +
+                ", surfaceArea=" + surfaceArea +
+                ", population=" + population +
+                ", capital='" + capital + '\'' +
+                '}';
+    }
+}
