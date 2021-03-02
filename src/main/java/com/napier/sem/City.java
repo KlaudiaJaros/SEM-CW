9fc35af282ffc68e8de69dac5e1b838682d6989f
anathalie <anathalievn@yahoo.co.uk>
2021-03-02 16:39:33 +0000

    added javadoc comments to city class

--- src/main/java/com/napier/sem/City.java
+++ src/main/java/com/napier/sem/City.java
@@ -10,46 +10,90 @@ public class City {
     private String district;
     private int population;
 
+    /**
+     * get the id of the city
+     * @return id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * sets id for the city
+     * @param id integer id of the city
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * gets the name of the city
+     * @return name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * sets the name of the city
+     * @param name string to be set as the name of the city
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * gets the country code of the country the city is in
+     * @return country code of the country code city is in
+     */
     public String getCountryCode() {
         return countryCode;
     }
 
+    /**
+     * sets the country code of the country the city is in
+     * @param countryCode string to be set as the country code
+     */
     public void setCountryCode(String countryCode) {
         this.countryCode = countryCode;
     }
 
+    /**
+     * gets the district the city is in
+     * @return district the city is in
+     */
     public String getDistrict() {
         return district;
     }
 
+    /**
+     * sets the district name the city is in
+     * @param district string to be set as the district the city is in
+     */
     public void setDistrict(String district) {
         this.district = district;
     }
 
+    /**
+     * gets population of the city
+     * @return population of the city
+     */
     public int getPopulation() {
         return population;
     }
 
+    /**
+     * sets the population of the city
+     * @param population integer to be set as the population of the city
+     */
     public void setPopulation(int population) {
         this.population = population;
     }
 
+    /**
+     * returns string description of the city
+     * @return string description of the city
+     */
     @Override
     public String toString() {
         return "City{" +
