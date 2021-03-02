94010c77fe8be22caf990b26bf9c3b20c1eec0ce
Klaudia <56263298+klaudiajaros@users.noreply.github.com>
2021-03-02 16:37:28 +0000

    Javadocs added for Country.java

--- src/main/java/com/napier/sem/Country.java
+++ src/main/java/com/napier/sem/Country.java
@@ -13,62 +13,122 @@ public class Country {
     private int population;
     private String capital;
 
+    /**
+     * Gets a country code.
+     * @return country code
+     */
     public String getCode() {
         return code;
     }
 
+    /**
+     * Sets the country code
+     * @param code string to be set as the country code
+     */
     public void setCode(String code) {
         this.code = code;
     }
 
+    /**
+     * Gets the country name.
+     * @return country name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of the country
+     * @param name string to be set as the name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets the continent of a country
+     * @return continent of a country
+     */
     public String getContinent() {
         return continent;
     }
 
+    /**
+     * Set the continent of a country
+     * @param continent string to be set as the continent
+     */
     public void setContinent(String continent) {
         this.continent = continent;
     }
 
+    /**
+     * Gets the region of the country.
+     * @return the region of the country
+     */
     public String getRegion() {
         return region;
     }
 
+    /**
+     * Sets the region of the country
+     * @param region string to be set as the region
+     */
     public void setRegion(String region) {
         this.region = region;
     }
 
+    /**
+     * Gets the surface of a country
+     * @return surface
+     */
     public double getSurfaceArea() {
         return surfaceArea;
     }
 
+    /**
+     * Sets the surface of a country
+     * @param surfaceArea double value to be set as the surface
+     */
     public void setSurfaceArea(double surfaceArea) {
         this.surfaceArea = surfaceArea;
     }
 
+    /**
+     * Gets the population of a country
+     * @return the population of the country
+     */
     public int getPopulation() {
         return population;
     }
 
+    /**
+     * Sets the population of a country
+     * @param population integer to be set as the population
+     */
     public void setPopulation(int population) {
         this.population = population;
     }
 
+    /**
+     * Gets the capital of a country
+     * @return country capital
+     */
     public String getCapital() {
         return capital;
     }
 
+    /**
+     * Sets the capital of the country
+     * @param capital string to be set as the capital of a country
+     */
     public void setCapital(String capital) {
         this.capital = capital;
     }
 
+    /**
+     * Returns a description of the country object with all its variables
+     * @return a String description of the Country object
+     */
     @Override
     public String toString() {
         return "Country{" +
