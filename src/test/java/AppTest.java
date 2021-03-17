5f91c339b6e0f925f5829ff913186368f31eb9c6
Patty <pattychr29@gmail.com>
2021-03-17 11:39:48 +0000

    updated travis file

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    @Test
+    void unitTest()
+    {
+        assertEquals(5, 5);
+    }
+}
