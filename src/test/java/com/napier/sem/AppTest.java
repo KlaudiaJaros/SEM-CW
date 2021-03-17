8700f57c7eda59b3df66650624413a531edc9505
Monika Sliciute <sliciute.monika@gmail.com>
2021-03-17 12:52:37 +0000

    Added unit tests

---
+++ src/test/java/com/napier/sem/AppTest.java
@@ -0,0 +1,18 @@
+package com.napier.sem;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    @Test
+    void unitTest()
+    {
+        assertEquals(5, 5);
+    }
+
+    @Test
+    void secondUnitTest()
+    {
+        assertEquals(1, 1);
+    }
+}
