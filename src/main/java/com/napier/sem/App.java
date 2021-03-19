e4d544c4364af87b399ecf91ac097013db485df8
Monika Sliciute <sliciute.monika@gmail.com>
2021-03-19 12:58:07 +0000

    updated travis.yml to test multiple classes, changed db location string to "localhost:3306"

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -21,7 +21,7 @@ public class App
 
         // connect and load data:
         //localhost:33060
-        db.connect("db:3306");
+        db.connect("localhost:3306");
         db.loadData();
 
         // Verify contents: check sizes - should be: 239, 4079, 984:
